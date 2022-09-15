ccd58cc8fd15c86feeef8dde72d7c6ac851b0ecc
TomBrown114514 <tomb0968@outlook.com>
2022-09-15 17:13:35 +0800

    homework of coding

---
+++ xt1_1_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("学号：\n");
+    printf("姓名：\n");
+    printf("班级：\n");
+    printf("E-mail：\n");
+    printf("微信：\n");
+    printf("QQ：\n");
+    return 0;
+}
