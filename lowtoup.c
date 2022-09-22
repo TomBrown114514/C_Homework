acf499df5933a3bfe2a498b671e45c0de3a62229
TomBrown114514 <tomb0968@outlook.com>
2022-09-22 20:03:58 +0800

    个工会经费股份回购

--- lowtoup.c
+++ lowtoup.c
@@ -4,9 +4,15 @@
 */
 int main(int argc, char const *argv[])
 {
-    printf("请输入你想转换的小写字母：");
+    printf("请输入你想转换的字母：");
     char a = getchar(); //获取键盘输入的字母
-    printf("%c 转换成大写字母的是",a);
-    putchar(a-32);
+    if (a >= 'a' && a <= 'z'){
+        printf("%c 转换成大写字母的是",a);
+        putchar(a-32);
+    } else if (a >= 'A' && a <= 'Z'){
+        printf("%c 转换成小写字母的是",a);
+        putchar(a+32);
+    } else printf("请重新输入！！");
     return 0;
 }
+
