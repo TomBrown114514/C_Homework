87f279a561c2ee3422240a54f81ed53c08627fa0
TomBrown114514 <tomb0968@outlook.com>
2022-09-22 20:22:19 +0800

    混合感染天花梵蒂冈地方

--- lowtoup.c
+++ lowtoup.c
@@ -6,10 +6,10 @@ int main(int argc, char const *argv[])
 {
     printf("请输入你想转换的字母：");
     char a = getchar(); //获取键盘输入的字母
-    if (a >= 'a' && a <= 'z'){
+    if (a >= 'a' && a <= 'z'){  //小写字母转大写字母
         printf("%c 转换成大写字母的是",a);
         putchar(a-32);
-    } else if (a >= 'A' && a <= 'Z'){
+    } else if (a >= 'A' && a <= 'Z'){ //大写字母转小写字母
         printf("%c 转换成小写字母的是",a);
         putchar(a+32);
     } else printf("请重新输入！！");
