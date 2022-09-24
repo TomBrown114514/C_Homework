183c68b6900ca502a1e9409f11870c275e1ac668
TomBrown114514 <tomb0968@outlook.com>
2022-09-24 20:50:50 +0800

    114514

--- yuan.c
+++ yuan.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#define Pi 3.14
+#define Pi 3.141592
+
 
 int main(){
     /*计算圆的面积和周长*/
     float r,s,c;
     printf("请输入圆的半径:");
     scanf("%f",&r);
-    s=Pi*r*r;
-    c=2*Pi*r;
+    s=Pi*r*r; /*面积*/
+    c=2*Pi*r; /*周长*/
     printf("圆的面积是%.2f，周长是%.2f",s,c);
     return 0;
 }
