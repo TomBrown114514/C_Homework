8bee518b3f4b41fa884cc0de56c601c1e86730b9
TomBrown114514 <tomb0968@outlook.com>
2022-09-25 19:04:29 +0800

    111

--- getavg.c
+++ getavg.c
@@ -4,12 +4,12 @@
  * 保留两位小数
 */
 int main(){
-    float socer,avg;
+    float score,avg;
     int i;
     printf("请输入3位同学的C语言成绩:");
     for(i=1;i<=3;i++){
-        scanf("%f",&socer);
-        avg=avg+socer;
+        scanf("%f",&score);
+        avg=avg+score;
     }
     avg=avg/3;
     printf("这三位C语言的平均成绩是:%.2f",avg);
